ecb1a6e844c68a124e2daf014426afa562703664
matterholt <matterholt.dev@gmail.com>
2022-04-06 18:31:56 -0400

    react and tailwinds

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
+import react from "@astrojs/react";
 
+import tailwind from "@astrojs/tailwind";
 
-  export default defineConfig({
-    integrations: [],
-  });
+// https://astro.build/config
+export default defineConfig({
+  integrations: [react(), tailwind()]
+});
